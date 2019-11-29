28b125f8a8d98cb1ed00f312f01bcf2102cb2276
Stuart Clove <stuart.clove@gmail.com>
2019-11-29 11:05:51 -0700

    Update longest-word-in-string.js

--- longest-word-in-string.js
+++ longest-word-in-string.js
@@ -4,20 +4,21 @@ const assert = require('assert');
 
 // Using reduce.
 function findLongestWordInString(string) {
+  if (!string || string === '') return '';
+
   // Remove numbers, and special characters (non-word) and return them as an array;
   // + sign makes it "greedy" so it returns the entire word match instead of each letter individually.
   const pattern = /[^\d\W]+/g;
   const matchArray = string.match(pattern);
 
-  return matchArray && matchArray.length
-    ? matchArray.reduce((a, b) => (a.length > b.length ? a : b))
-    : '';
+  return matchArray.reduce((a, b) => (a.length > b.length ? a : b));
 }
 
 assert.equal(
   findLongestWordInString('This is aasdfasdfasdf, rando2320f--2, dasdf;.'),
   'aasdfasdfasdf'
 );
+assert.equal(findLongestWordInString(''), '');
 console.log('All tests passed!');
 
 // Using older style way with for loop.
